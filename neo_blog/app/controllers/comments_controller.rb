be98560ad390b26e09480042ab6d89d3b2ddab45
Shane Rogers <rogerssh@tcd.ie>
2013-05-30 19:53:52 -0600

    Comment relationship in Comment#create

--- neo_blog/app/controllers/comments_controller.rb
+++ neo_blog/app/controllers/comments_controller.rb
@@ -40,7 +40,9 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(params[:comment])
+    @post = Post.find(params[:id])
+    @comment = @post.comments.build(params[:comment])
+
 
     respond_to do |format|
       if @comment.save
