73426ed6058d8d0753694ef03ea76346bdf66bf2
Shane Rogers <rogerssh@tcd.ie>
2013-05-30 19:24:23 -0600

    neo_blog created

---
+++ neo_blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeoBlog::Application.config.secret_token = '2e5ea59bb860bc9728832f14dd1b12ed84c3b536d24ce25f9084dca1abbf1263ea054533416ce402a6c4d908274e6513b77b85c15a1ee9f9576cb55112456e27'
